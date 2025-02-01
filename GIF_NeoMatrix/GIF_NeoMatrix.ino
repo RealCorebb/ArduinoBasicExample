a99e068d7b9eb1df8a916fe4771f6f6d4d6dc4ee
RealCorebb <33654314+RealCorebb@users.noreply.github.com>
2025-02-01 13:44:24 +0800

    Fix XY

--- GIF_NeoMatrix/GIF_NeoMatrix.ino
+++ GIF_NeoMatrix/GIF_NeoMatrix.ino
@@ -4,7 +4,7 @@
 #include <LittleFS.h>
 
 // Pin configuration
-#define PIN        14
+#define PIN 6
 #define MATRIX_WIDTH 8
 #define MATRIX_HEIGHT 8
 #define NUMPIXELS (MATRIX_WIDTH * MATRIX_HEIGHT)
@@ -16,33 +16,29 @@ AnimatedGIF gif;
 #define MAX_GIF_SIZE 32768
 #define MAX_FILENAME_LEN 32
 
-uint8_t* gifBuffer = nullptr;
+uint8_t *gifBuffer = nullptr;
 size_t gifSize = 0;
-char currentGifFile[MAX_FILENAME_LEN] = "/1.gif";  // Default GIF file
+char currentGifFile[MAX_FILENAME_LEN] = "/105.gif";  // Default GIF file
 bool needToLoadNewGif = true;
 
 uint16_t XY(uint8_t x, uint8_t y) {
-  if (y & 1) {
-    uint8_t reverseX = (MATRIX_WIDTH - 1) - x;
-    return (y * MATRIX_WIDTH) + reverseX;
-  } else {
-    return (y * MATRIX_WIDTH) + x;
-  }
+  return (y * MATRIX_WIDTH) + x;
 }
+
 
 void GIFDraw(GIFDRAW *pDraw) {
   uint8_t r, g, b, *s, *p, *pPal = (uint8_t *)pDraw->pPalette;
   int x, y = pDraw->iY + pDraw->y;
 
   if (y >= MATRIX_HEIGHT) return;
-  
+
   s = pDraw->pPixels;
   if (pDraw->ucDisposalMethod == 2) {
     p = &pPal[pDraw->ucBackground * 3];
     r = p[0] >> BRIGHT_SHIFT;
     g = p[1] >> BRIGHT_SHIFT;
     b = p[2] >> BRIGHT_SHIFT;
-    
+
     for (x = 0; x < pDraw->iWidth && x < MATRIX_WIDTH; x++) {
       if (s[x] == pDraw->ucTransparent) {
         pixels.setPixelColor(XY(x, y), pixels.Color(r, g, b));
@@ -56,17 +52,17 @@ void GIFDraw(GIFDRAW *pDraw) {
     for (x = 0; x < pDraw->iWidth && x < MATRIX_WIDTH; x++) {
       if (s[x] != ucTransparent) {
         p = &pPal[s[x] * 3];
-        pixels.setPixelColor(XY(x, y), pixels.Color(p[0]>>BRIGHT_SHIFT, p[1]>>BRIGHT_SHIFT, p[2]>>BRIGHT_SHIFT));
+        pixels.setPixelColor(XY(x, y), pixels.Color(p[0] >> BRIGHT_SHIFT, p[1] >> BRIGHT_SHIFT, p[2] >> BRIGHT_SHIFT));
       }
     }
   } else {
     for (x = 0; x < pDraw->iWidth && x < MATRIX_WIDTH; x++) {
       p = &pPal[s[x] * 3];
-      pixels.setPixelColor(XY(x, y), pixels.Color(p[0]>>BRIGHT_SHIFT, p[1]>>BRIGHT_SHIFT, p[2]>>BRIGHT_SHIFT));
+      pixels.setPixelColor(XY(x, y), pixels.Color(p[0] >> BRIGHT_SHIFT, p[1] >> BRIGHT_SHIFT, p[2] >> BRIGHT_SHIFT));
     }
   }
 
-  if (pDraw->y == pDraw->iHeight-1) {
+  if (pDraw->y == pDraw->iHeight - 1) {
     pixels.show();
   }
 }
@@ -93,21 +89,21 @@ void checkSerial() {
   if (Serial.available()) {
     String command = Serial.readStringUntil('\n');
     command.trim();
-    
+
     if (command.startsWith("gif:")) {
       String filename = command.substring(4);
       filename.trim();
-      
+
       // Add leading slash if not present
       if (!filename.startsWith("/")) {
         filename = "/" + filename;
       }
-      
+
       // Add .gif extension if not present
       if (!filename.endsWith(".gif")) {
         filename += ".gif";
       }
-      
+
       // Check if file exists
       if (LittleFS.exists(filename)) {
         strncpy(currentGifFile, filename.c_str(), MAX_FILENAME_LEN - 1);
@@ -125,7 +121,7 @@ void setup() {
   Serial.begin(115200);
   Serial.println("\nESP32 GIF Player");
   Serial.println("Send 'gif:filename' to change GIF (e.g., 'gif:2' for /2.gif)");
-  
+
   pixels.begin();
   pixels.clear();
   pixels.show();
@@ -135,7 +131,7 @@ void setup() {
     return;
   }
 
-  gifBuffer = (uint8_t*)malloc(MAX_GIF_SIZE);
+  gifBuffer = (uint8_t *)malloc(MAX_GIF_SIZE);
   if (!gifBuffer) {
     Serial.println("Failed to allocate GIF buffer");
     return;
@@ -150,7 +146,7 @@ void loop() {
   if (needToLoadNewGif) {
     if (loadGifFile()) {
       needToLoadNewGif = false;
-      gif.close(); // Close any previously opened GIF
+      gif.close();  // Close any previously opened GIF
     } else {
       delay(1000);
       return;
@@ -160,7 +156,7 @@ void loop() {
   int rc = gif.open(gifBuffer, gifSize, GIFDraw);
   if (rc) {
     while (rc && !needToLoadNewGif) {
-      checkSerial(); // Check for new commands between frames
+      checkSerial();  // Check for new commands between frames
       rc = gif.playFrame(true, NULL);
       if (!rc) {
         // End of animation reached, reopen for looping
